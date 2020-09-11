6f5dd867a844fb84e5104ebfe128e9f2d1afd12d
dotslashqq <tomcarlo.lim@gmail.com>
2020-09-11 14:56:14 +0800

    Updates

---
+++ precache-manifest.c238e7d2f2748e5066de652becd16649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c067b90098f858d1241d93bcf297509b",
+    "url": "/alan-ai-project/index.html"
+  },
+  {
+    "revision": "ad2fb7ba6f18e148c5ce",
+    "url": "/alan-ai-project/static/css/main.c84c91a9.chunk.css"
+  },
+  {
+    "revision": "4dd5a6e5817e704f20cb",
+    "url": "/alan-ai-project/static/js/2.e4c94d69.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/alan-ai-project/static/js/2.e4c94d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2fb7ba6f18e148c5ce",
+    "url": "/alan-ai-project/static/js/main.65bd237e.chunk.js"
+  },
+  {
+    "revision": "a8d4c11e4f66b10c77c9",
+    "url": "/alan-ai-project/static/js/runtime-main.ca74aef9.js"
+  }
+]);
